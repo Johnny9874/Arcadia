c73a629c3eb066d74de14b6338b23230d5e2fc27
Johnny9874 <ye.johnnypro@gmail.com>
2024-10-15 16:37:22 +0200

    1er commit

--- Page_principale.js
+++ Page_principale.js
@@ -1,1 +1,15 @@
-console.log('bienvenue');
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.carousel-slide');
+    const totalSlides = slides.children.length;
+
+    // Mise à jour de l'index actuel
+    currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+
+    // Calculer le pourcentage de translation
+    const offset = -(currentIndex * 100 / totalSlides);
+    
+    // Appliquer la translation
+    slides.style.transform = 'translateX(' + offset + '%)';
+}
