bf8e95fceea1e572ef7a0aba182a27051c7010bb
Johnny9874 <ye.johnnypro@gmail.com>
2024-11-19 13:48:19 +0100

    dernier commit 2

---
+++ public/sql/insertData.sql
@@ -0,0 +1,7 @@
+-- Insert data dans la table administrateur
+INSERT INTO public.admin (username, password) VALUES
+('Admin', 'AG)pbce]/mAzm5Fg')
+
+-- Insert data dans la table employé
+INSERT INTO public.employé (username, password) VALUES
+('lea', '123456789')
